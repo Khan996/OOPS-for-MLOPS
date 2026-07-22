68a9fc0593f171ca82c227622d908c14a471dec3
Babar <babarnadeem996@gmail.com>
2026-07-22 14:24:16 +0500

    sign in method created

--- oops_proj.py
+++ oops_proj.py
@@ -15,7 +15,7 @@ class chatbook:
         if user_input == "1":
             self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input ==4:
@@ -29,5 +29,20 @@ class chatbook:
         self.username = email
         self.password = pwd
         self.menu()
+
+    def signin(self):
+        if self.username == "" and self.password == "":
+            print("Please sign up first by entering your username and password \n")
+        else:
+            uname = input("Please enter your email/username: ")
+            pwd = input("Please enter your password: ")
+            if self.username == uname and self.password == pwd:
+                print("You have signed in successfully!\n")
+                self.loggedin = True
+            else:
+                print("Please input correct credentials. \n")
+        self.menu()
+
+        
 
 obj = chatbook()
